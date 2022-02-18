df3488e715006aeda9b9918c77510875411aa3a5
wanderfulife <johm.wander@hotmail.fr>
2022-02-19 00:58:46 +1100

    deployed

--- nft-collectible/hardhat.config.js
+++ nft-collectible/hardhat.config.js
@@ -1,4 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
+
+const { API_URL, PRIVATE_KEY, ETHERSCAN_API } = process.env;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +22,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "rinkeby",
+  networks: {
+    rinkeby: {
+      url: API_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API
+  }
 };
