f76305d3a9646dca10c0852ac54a1a9e89f1431e
wanderfulife <john.wander@hotmail.fr>
2022-02-20 13:38:21 +1100

    frontend

---
+++ nft-collectible-frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import contract from './contracts/TheSquirrelz.json';
+
+const contractAddress = "0x13C8b8b85098C4F91947Ffd0b64BACCfadCE8efE";
+const abi = contract.abi;
+
+function App() {
+  return (
+   <h1>Hello World</h1>
+  );
+}
+
+export default App;
